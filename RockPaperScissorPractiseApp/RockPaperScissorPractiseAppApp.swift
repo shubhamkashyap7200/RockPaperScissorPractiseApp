4d8120af87f080c721e5c5755daf77e035a99c48
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-01 15:15:38 +0530

    Initial Commit

---
+++ RockPaperScissorPractiseApp/RockPaperScissorPractiseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorPractiseAppApp.swift
+//  RockPaperScissorPractiseApp
+//
+//  Created by Shubham on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorPractiseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
